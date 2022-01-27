baee6a62308e8e34ca3acc849bc1471c9dadc8ff
HornOfSaltff <ecy14@scarletmail.rutgers.edu>
2022-01-27 17:18:28 -0500

    Write a CameraController script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,12 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 0;
 
     private Rigidbody rb;
+
+    private float movementX;
+    private float movementY;
 
     // Start is called before the first frame update
     void Start()
@@ -14,14 +18,19 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    void OnMove(InputValue movementValue)
+    private void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
     }
 
 }
